5aeedb1c17e64b39b11dac56cda169cb4e2705e3
tanzid64 <tanzid3@gmail.com>
2026-02-28 11:33:55 +0600

    Enhances tab navigation and screen layouts with consistent styling and color scheme

--- src/app/(tabs)/favorites.tsx
+++ src/app/(tabs)/favorites.tsx
@@ -1,9 +1,17 @@
+import { Colors } from "@/constants/colors";
 import { Text, View } from "react-native";
 
 export default function FavoritesScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Favorites</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: Colors.background,
+      }}
+    >
+      <Text style={{ color: Colors.text, fontSize: 16 }}>Favorites</Text>
     </View>
   );
 }
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,17 @@
+import { Colors } from "@/constants/colors";
 import { Text, View } from "react-native";
 
 export default function PokedexScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Pokedex</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: Colors.background,
+      }}
+    >
+      <Text style={{ color: Colors.text, fontSize: 16 }}>Pokédex</Text>
     </View>
   );
 }
